4d173a6f9fb95f56ef19c66d19a5900f82e9870e
gowtham-source <75899822+gowtham-source@users.noreply.github.com>
2023-03-15 22:29:31 +0530

    readme

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,29 @@ from PIL import Image
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+# st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+st.write("[![Instagram](https://img.shields.io/badge/Instagram-%40Gowtham-orange?logo=instagram)](https://www.instagram.com/mystical_boy_25/)")
 
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('Gowtham M, B Tech Ai & Ds✨')
 
 st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('youtube', 'https://youtube.com/dataprofessor',
+          'Data Professor YouTube channel', icon_size)
+st_button('youtube', 'https://youtube.com/codingprofessor',
+          'Coding Professor YouTube channel', icon_size)
+st_button('medium', 'https://data-professor.medium.com/',
+          'Read my Blogs', icon_size)
+st_button('twitter', 'https://twitter.com/thedataprof/',
+          'Follow me on Twitter', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/',
+          'Follow me on LinkedIn', icon_size)
+st_button('newsletter', 'https://sendfox.com/dataprofessor/',
+          'Sign up for my Newsletter', icon_size)
+st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/',
+          'Buy me a Coffee', icon_size)
